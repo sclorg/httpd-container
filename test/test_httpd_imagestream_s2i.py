54f5b007b99e1c563190e5bb94e9f25bf6853209
Petr "Stone" Hracek <phracek@redhat.com>
2025-03-31 13:11:34 +0200

    S2I Imagestreams does not work in Shared Cluster.
    Let's disable it for know. As soon as build is done,
    then deployment is not caught the new build image.
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_httpd_imagestream_s2i.py
+++ test/test_httpd_imagestream_s2i.py
@@ -20,7 +20,7 @@ class TestHTTPDImagestreamS2I:
 
     def setup_method(self):
         self.template_name = get_service_image(IMAGE_NAME)
-        self.oc_api = OpenShiftAPI(pod_name_prefix=self.template_name, version=VERSION, shared_cluster=True)
+        self.oc_api = OpenShiftAPI(pod_name_prefix=self.template_name, version=VERSION)
 
     def teardown_method(self):
         self.oc_api.delete_project()
@@ -33,9 +33,10 @@ class TestHTTPDImagestreamS2I:
             imagestream_file=f"imagestreams/httpd-{os_name}.json",
             image_name=IMAGE_NAME,
             app="https://github.com/sclorg/httpd-container.git",
-            context="examples/sample-test-app"
+            context="examples/sample-test-app",
+            service_name=self.template_name
         )
-        assert self.oc_api.template_deployed(name_in_template=self.template_name)
+        assert self.oc_api.is_s2i_pod_running(pod_name_prefix=self.template_name)
         assert self.oc_api.check_response_inside_cluster(
             name_in_template=self.template_name, expected_output="This is a sample s2i application with static content"
         )
