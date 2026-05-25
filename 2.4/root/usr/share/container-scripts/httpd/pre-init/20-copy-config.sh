5570e915974a6bb3bb71b343c5343fe34a0909bd
Nikola Davidova <ndavidov@redhat.com>
2026-05-25 13:56:40 +0200

    add distgen support

--- 2.4/root/usr/share/container-scripts/httpd/pre-init/20-copy-config.sh
+++
@@ -1,4 +0,0 @@
-source ${HTTPD_CONTAINER_SCRIPTS_PATH}/common.sh
-
-# Copy config files from application to the location where httd expects them
-process_config_files ${HTTPD_APP_ROOT}/src
--- 2.4/root/usr/share/container-scripts/httpd/pre-init/40-ssl-certs.sh
+++
@@ -1,4 +0,0 @@
-source ${HTTPD_CONTAINER_SCRIPTS_PATH}/common.sh
-
-# Copy SSL files provided in application source
-process_ssl_certs ${HTTPD_APP_ROOT}/src
